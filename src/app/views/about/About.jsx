85fa6ef3fbc8af9ce65c146347effca1afe565ba
MacKentoch <erwan.datin@gmail.com>
2016-04-02 17:59:26 +0200

    chore : about view

--- src/app/views/about/About.jsx
+++ src/app/views/about/About.jsx
@@ -15,9 +15,30 @@ class About extends React.Component {
         key="homeView">
         <ViewContainer
           isAnimated={true}>
-          <h1>
-            About view
-          </h1>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="jumbotron about_view_jumbotron">
+                <h1>
+                  Why this view?
+                </h1>
+                <p>
+                  A single page application without client side routing would not be a single page.
+                </p>
+                <p>
+                  So this View is just an excuse to use react-router.
+                </p>
+                <p>
+                  <a
+                    className="btn btn-primary btn-lg"
+                    href="https://github.com/MacKentoch">
+                    See more in my github
+                    &nbsp;
+                    <i className="fa fa-github"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
         </ViewContainer>
       </div>
     );
